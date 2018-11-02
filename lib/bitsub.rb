9ddacc7972d7f48448b79a686e7d1d99b6a37449
Stone Tickle <lattis@mochiro.moe>
2018-11-01 21:00:14 -0500

    a barebones rss feed scaper

--- lib/bitsub.rb
+++ lib/bitsub.rb
@@ -1,6 +1,38 @@
+require 'rss'
 require 'bitsub/version'
 
-module Bitsub
-  class Error < StandardError; end
-  # Your code goes here...
+module BitSub
+  module_function
+
+  def make_sub(pat, *dir)
+    { pat: pat, dir: File.join(*dir) }
+  end
+
+  def find_all(subs)
+    lambda do |feed|
+      subs.map do |s|
+        find(feed, s[:pat]).map do |item|
+          {
+            title: item.title.strip,
+            link: item.link.strip,
+            date: item.pubDate,
+            desc: item.description&.strip || '',
+            source: item.source,
+            comments: item.comments,
+            author: item.author,
+            category: item.category,
+            download_dir: s[:dir],
+            file:  File.join(s[:dir], item.title.strip),
+            downloaded: File.exist?(File.join(s[:dir], item.title.strip))
+          }
+        end
+      end.flatten
+    end
+  end
+
+  def find(rss, pat)
+    RSS::Parser.parse(rss)
+               .items
+               .select { |item| pat.match?(item.title) }
+  end
 end

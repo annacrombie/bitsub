314ed17622c4139684362e5fd418e41b4c946371
Stone Tickle <lattis@mochiro.moe>
2020-02-25 10:12:02 +0900

    add shortcuts for common use-cases

--- lib/bitsub/dsl.rb
+++ lib/bitsub/dsl.rb
@@ -9,8 +9,19 @@ module BitSub
       @settings = {
         output_fmt: lambda { |res| res[:link] },
         output_sep: "\n",
-        reject: lambda { |t| t[:downloaded] }
+        reject: ->(t){ false }
       }
+    end
+
+    def reject(r)
+      case r
+      when Proc
+        setting(reject: r)
+      when :downloaded
+        setting(reject: lambda { |t| t[:downloaded] })
+      else
+        raise "Invalid filter"
+      end
     end
 
     def dir(dir)
@@ -23,6 +34,37 @@ module BitSub
 
     def feed(uri)
       @feeds << uri
+    end
+
+    def nyaa(title, subgroup, res: '720')
+      keyword = title.split.first.downcase
+
+      sub(title, /#{subgroup}.*#{keyword}.*#{res}/i)
+      feed("https://nyaa.si/?page=rss&c=1_2&f=1&q=#{subgroup}+#{keyword}")
+    end
+
+    def output(type)
+      case type
+      when :transmission
+        setting(output_fmt: lambda do |t|
+          ['--add', t[:link], '--download-dir', t[:download_dir]].join("\0")
+        end)
+
+        setting(output_sep: "\0")
+      when :pretty
+        setting(output_fmt: lambda do |t|
+          sprintf(
+            "%s - \e[%dm%s\e[0m",
+            t[:date].strftime("%y.%m.%d"),
+            t[:downloaded] ? 32 : 31,
+            t[:title],
+          )
+        end)
+
+        setting(output_sep: "\n")
+      else
+        raise "Unknown output type #{type}"
+      end
     end
 
     def setting(hash)
